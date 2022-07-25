a60af0e6d4c0eac813369c6b793288c16fd3df10
dominggo1999 <arnolddominggo19@gmail.com>
2022-07-25 21:21:26 +0700

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,40 @@ import { Redirect, Route, useHistory } from 'react-router-dom';
 
 setupIonicReact();
 
-function HomePage() {
+const DetailPage = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton className="text-blue-500" />
+          </IonButtons>
+          <IonTitle>Detail Page</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <h1 className="text-2xl font-bold mt-3">DETAIL</h1>
+        <IonItem details>
+          <IonLabel>More Information</IonLabel>
+        </IonItem>
+        <IonItem details>
+          <IonLabel>More Information</IonLabel>
+        </IonItem>
+        <IonItem details>
+          <IonLabel>More Information</IonLabel>
+        </IonItem>
+        <IonItem details>
+          <IonLabel>More Information</IonLabel>
+        </IonItem>
+        <IonItem details>
+          <IonLabel>More Information</IonLabel>
+        </IonItem>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+const Homepage = () => {
   const history = useHistory();
   const nextPage = () => {
     history.push('/detail');
@@ -27,54 +60,46 @@ function HomePage() {
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Makan Bang</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <h1>HOME PAGE</h1>
-        <IonButton onClick={nextPage}>NEXT PAGE</IonButton>
+      <IonContent>
+        <div className="w-full h-screen items-center flex flex-col justify-center bg-black text-white">
+          <p className="text-2xl">Placeholder</p>
+          <IonButton
+            className="mt-3"
+            onClick={nextPage}
+          >
+            Next Page
+          </IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
-}
+};
 
-function DetailPage() {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonBackButton />
-          </IonButtons>
-          <IonTitle>Detail Page</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <h1>DETAIL</h1>
-        <IonItem details>
-          <IonLabel>More Information</IonLabel>
-        </IonItem>
-      </IonContent>
-    </IonPage>
-  );
-}
-
-function App() {
+const App = () => {
   return (
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet>
-          <Route path="/home" component={HomePage} exact />
-          <Route path="/detail" component={DetailPage} exact />
-          <Route path="/" exact>
+          <Route
+            path="/home"
+            component={Homepage}
+            exact
+          />
+          <Route
+            path="/detail"
+            component={DetailPage}
+            exact
+          />
+          <Route
+            path="/"
+            exact
+          >
             <Redirect to="/home" />
           </Route>
         </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
   );
-}
+};
 
 export default App;
